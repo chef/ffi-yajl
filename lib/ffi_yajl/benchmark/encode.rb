76df4a48325ac983362354e8960639881a2e6d12
Lamont Granquist <lamont@scriptkiddie.org>
2013-11-17 23:03:19 -0800

    ruby 1.8 doesn't define RUBY_ENGINE

--- lib/ffi_yajl/benchmark/encode.rb
+++ lib/ffi_yajl/benchmark/encode.rb
@@ -4,7 +4,7 @@
 require 'rubygems'
 require 'benchmark'
 require 'stringio'
-unless RUBY_ENGINE =~ /jruby/
+if !defined?(RUBY_ENGINE) || RUBY_ENGINE !~ /jruby/
   begin
     require 'yajl'
   rescue Exception
