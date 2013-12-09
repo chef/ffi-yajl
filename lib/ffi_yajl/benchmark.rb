58da300fcf449b713905b886461fab4154df41b1
Lamont Granquist <lamont@scriptkiddie.org>
2013-12-08 22:46:57 -0800

    adding new benchmarks

--- lib/ffi_yajl/benchmark.rb
+++ lib/ffi_yajl/benchmark.rb
@@ -1,5 +1,7 @@
 
 require 'ffi_yajl/benchmark/encode.rb'
 require 'ffi_yajl/benchmark/encode_profile.rb'
-#require 'ffi_yajl/benchmark/parse.rb'
+require 'ffi_yajl/benchmark/parse.rb'
+require 'ffi_yajl/benchmark/parse_profile.rb'
+require 'ffi_yajl/benchmark/parse_profile_ruby_prof.rb'
 
