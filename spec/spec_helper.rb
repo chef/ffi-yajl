792ad6b2d8157b4ebd6c2c5ad1fd2ea798d2d415
Lamont Granquist <lamont@scriptkiddie.org>
2013-12-22 23:38:02 -0800

    fix spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,9 +3,7 @@ $: << File.expand_path(File.join(File.dirname( __FILE__ ), "../lib"))
 require 'ffi_yajl'
 
 RSpec.configure do |c|
-  c.treat_symbols_as_metadata_keys_with_true_values = true
-  c.run_all_when_everything_filtered = true
-  c.filter_run :focus
+  c.filter_run_excluding :ruby_gte_19 => true unless RUBY_VERSION.to_f >= 1.9
 
   c.order = 'random'
 
@@ -13,5 +11,4 @@ RSpec.configure do |c|
     c.syntax = :expect
   end
 
-  c.filter_run_excluding :ruby_gte_19 => true unless RUBY_VERSION.to_f >= 1.9
 end
