bebfe63aaa939e63b6b0871df3186be02b028256
Lamont Granquist <lamont@scriptkiddie.org>
2014-06-04 23:09:33 -0700

    use fiddle instead of DL if we find it

--- lib/ffi_yajl/ext.rb
+++ lib/ffi_yajl/ext.rb
@@ -2,10 +2,13 @@ require 'rubygems'
 
 require 'ffi_yajl/encoder'
 require 'ffi_yajl/parser'
-require 'dl'
 require 'ffi'
 require 'libyajl2'
-
+begin
+  require 'fiddle'
+rescue LoadError
+  require 'dl'
+end
 
 module FFI_Yajl
   class Parser
@@ -13,7 +16,11 @@ module FFI_Yajl
     libname = ::FFI.map_library_name("yajl")
     libpath = File.expand_path(File.join(Libyajl2.opt_path, libname))
     libpath.gsub!(/dylib/, 'bundle')
-    ::DL.dlopen(libpath)
+    if defined?(Fiddle)
+      ::Fiddle.dlopen(libpath)
+    else
+      ::DL.dlopen(libpath)
+    end
     require 'ffi_yajl/ext/parser'
     include FFI_Yajl::Ext::Parser
   end
@@ -23,7 +30,11 @@ module FFI_Yajl
     libname = ::FFI.map_library_name("yajl")
     libpath = File.expand_path(File.join(Libyajl2.opt_path, libname))
     libpath.gsub!(/dylib/, 'bundle')
-    ::DL.dlopen(libpath)
+    if defined?(Fiddle)
+      ::Fiddle.dlopen(libpath)
+    else
+      ::DL.dlopen(libpath)
+    end
     require 'ffi_yajl/ext/encoder'
     include FFI_Yajl::Ext::Encoder
   end
