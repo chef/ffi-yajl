39a19644e678b7ac24808be203895f5f758a1e6d
Lamont Granquist <lamont@scriptkiddie.org>
2014-10-09 19:18:12 -0700

    the c ext loads and still tests fine on 1.8.7

--- lib/ffi_yajl.rb
+++ lib/ffi_yajl.rb
@@ -1,8 +1,7 @@
 
 if ENV['FORCE_FFI_YAJL'] == "ext"
   require 'ffi_yajl/ext'
-elsif ENV['FORCE_FFI_YAJL'] == "ffi" || defined?(Yajl) || RUBY_VERSION.to_f < 1.9
-  # - can't dynlink our libyajl2 c-ext and Yajl's libyajl1 c-ext into the same binary
+elsif ENV['FORCE_FFI_YAJL'] == "ffi" || defined?(Yajl)
   # - c-extension segfaults on ruby 1.8.7
   require 'ffi_yajl/ffi'
 else
