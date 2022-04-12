2988001d6db3078119346964bb1ccae895520bf4
skeshari12 <skeshari@msystechnologies.com>
2022-04-12 18:04:45 +0530

    add again spec for jruby
    Signed-off-by: skeshari12 <skeshari@msystechnologies.com>

--- .expeditor/buildkite/run_windows_tests.ps1
+++ .expeditor/buildkite/run_windows_tests.ps1
@@ -4,17 +4,21 @@ $ErrorActionPreference = "Stop"
 # This will run ruby test on windows platform
 
 echo "--- Install make "
-choco install make --source=cygwin 
+choco install make --source=cygwin
+If ($lastexitcode -ne 0) { Exit $lastexitcode }
 
 Write-Output "--- Bundle install"
 ruby --version
 bundler --version
 gem update --system
+If ($lastexitcode -ne 0) { Exit $lastexitcode }
 bundle install --without development_extras --jobs 3 --retry 3 --path vendor/bundle
+If ($lastexitcode -ne 0) { Exit $lastexitcode }
 gem install yajl-ruby json psych
 If ($lastexitcode -ne 0) { Exit $lastexitcode }
 
 Write-Output "--- Bundle Execute"
 bundle exec rake compile
+If ($lastexitcode -ne 0) { Exit $lastexitcode }
 bundle exec rake spec
 If ($lastexitcode -ne 0) { Exit $lastexitcode }
