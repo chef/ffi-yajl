164fca12782575349e76b024d1c880df57de1e3f
Marc A. Paradise <marc.paradise@gmail.com>
2022-04-12 11:58:45 -0400

    Use build tools that are included with devkit
    These are in the devkit that is part of the base image and are
    guaranteed correct for native compilation of gems against these versions
    of ruby.
    
    Signed-off-by: Marc A. Paradise <marc.paradise@gmail.com>

--- .expeditor/buildkite/run_windows_tests.ps1
+++ .expeditor/buildkite/run_windows_tests.ps1
@@ -1,24 +1,42 @@
-# Stop script execution when a non-terminating error occurs
+param([String]$version)
+# This script will run ruby test on windows platform. It requires a version
+# "3.0" or "3.1" as an argument.
+
+# Stop script execution when a non-terminating error occurs. Note that this makes it
+# unneccesary to check the exit code of each program being run - non-zero exit will force it to fail and terminate.
 $ErrorActionPreference = "Stop"
 
-# This will run ruby test on windows platform
+# The specific paths of tools within the ruby30/31 devkit vary a bit across 3.0 and 3.1
+if ($version -eq "3.0")
+{
+    $base_dir = "C:\ruby30\"
+    $Env:PATH += ";" + $base_dir + "ruby\bin;" + $base_dir + "msys64\usr\bin;" + $base_dir + "msys64\mingw64\bin"
+}
+elseif($version -eq "3.1")
+{
+    $base_dir = "C:\ruby31\"
+    # Note path change - gcc is living in ucrt64\bin here, and mingw64 in earlier versions.
+    $Env:PATH += ";" + $base_dir + "ruby\bin;" + $base_dir + "msys64\usr\bin;" + $base_dir + "msys64\ucrt64\bin"
+}
 
-echo "--- Install make "
-choco install make --source=cygwin
-If ($lastexitcode -ne 0) { Exit $lastexitcode }
-
-Write-Output "--- Bundle install"
+Write-Output "--- Ensuring required bins are in path"
+Write-Output  "PATH: " + $Env:PATH
+make --version
+gcc --version
 ruby --version
 bundler --version
-gem update --system
-If ($lastexitcode -ne 0) { Exit $lastexitcode }
-bundle install --without development_extras --jobs 3 --retry 3 --path vendor/bundle
-If ($lastexitcode -ne 0) { Exit $lastexitcode }
-gem install yajl-ruby json psych
-If ($lastexitcode -ne 0) { Exit $lastexitcode }
 
-Write-Output "--- Bundle Execute"
+Write-Output "--- Updating system gems"
+gem update --system
+
+Write-Output "--- Bundle install"
+bundle install --without development_extras --jobs 3 --retry 3 --path vendor/bundle
+
+Write-Output "--- Gem install"
+gem install yajl-ruby json psych
+
+Write-Output "--- Bundle Execute: rake compile"
 bundle exec rake compile
-If ($lastexitcode -ne 0) { Exit $lastexitcode }
+
+Write-Output "--- Bundle Execute: rake spec"
 bundle exec rake spec
-If ($lastexitcode -ne 0) { Exit $lastexitcode }
