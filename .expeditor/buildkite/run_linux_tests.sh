7ccfed7d9516ffec124e097b137492c1853ab2f0
Thomas Powell <thomas.powell@progress.com>
2025-02-06 12:08:21 -0500

    Fix 3.0 and drop 2.6
    Signed-off-by: Thomas Powell <thomas.powell@progress.com>

--- .expeditor/buildkite/run_linux_tests.sh
+++ .expeditor/buildkite/run_linux_tests.sh
@@ -6,7 +6,10 @@ set -evx
 echo "---Bundle install---"
 ruby --version
 bundle --version
-gem update --system
+if [[ ! `ruby --version` !~ "ruby 3.0" ]]
+then
+  gem update --system
+fi
 bundle install --without development_extras --jobs 3 --retry 3 --path vendor/bundle
 gem install yajl-ruby json psych
 
